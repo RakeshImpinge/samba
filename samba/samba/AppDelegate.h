8a6f2ee690e0c86b5ba0282a966cc4ae520f8500
rakesh <rakesh.impinge@gmail.com>
2012-11-20 20:27:56 +0530

    fdfdfdf
    fdsf

---
+++ samba/samba/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  samba
+//
+//  Created by apple on 20/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
